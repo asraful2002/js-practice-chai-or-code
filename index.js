7ab64f842b6cf3b1195d6c4c3ea98b160535107e
Asraful Islam <asraful2002@example.com>
2026-05-07 00:52:50 -0700

    hellow

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(undefined == 0);
+// console.log(undefined == 0);
+
+// const id = symbol('123')
+// const anotherId = symbol('123')
+// console.log(id == anotherId);
+
+// const bigNumber = 244248548n
+
+// const heros =["asraful","islam","jakariya"]{
+//   name: "ayan",
+//   age: 2,
+// }
+// const myFuncion = function(){
+//   console.log(("hellow world"));
+  
+// }
+
+let name = "asraful islam";
+let anotherName = name;
+anotherName = "jakariya"
+
+console.log(name);
+console.log(anotherName);
+
+let userOne = {
+  email:"user@google.com"
+}
